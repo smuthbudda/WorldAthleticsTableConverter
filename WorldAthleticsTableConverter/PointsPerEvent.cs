15aff6a7b0fbba79ec480fbd4ed21f27ff41f753
Jordan Samson <jkdsamson@outlook.com>
2023-03-05 20:25:38 +1100

    Add project files.

---
+++ WorldAthleticsTableConverter/PointsPerEvent.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+
+namespace WorldAthleticsTableConverter;
+
+public class PointsPerEvent
+{
+    public int Points { get; set; }
+    public Gender Gender { get; set; }
+    public Category Category { get; set; }
+    public string Event { get; set; }
+    public decimal Mark { get; set; }
+}
+
+static class Events
+{
+    public static readonly string[] OutdoorEvents =
+    { "10 Miles","10,000m","10,000mW","1000m","100km","100m","100mH","10km","10kmW","110mH",
+        "15,000mW","1500m","15km","15kmW","2 Miles","20,000mW","2000m","2000mSC","200m","20km","20kmW",
+        "25km","30,000mW","3000m","3000mSC","3000mW","300m","30km","30kmW","35,000mW","35kmW","3kmW","400m","400mH",
+        "4x100m","4x200m","4x400m","50,000mW","5000m","5000mW","500m","50kmW","5km","5kmW","600m","800m","DT",
+        "Heptathlon","Decathlon","HJ","HM","HT","JT","LJ","Marathon","Mile","PV","SP", "TJ"
+    };
+    public static readonly string[] IndoorEvents =
+    { "50m" ,"55m","60m","50mH","55mH","60mH","Pentathlon"
+    };
+
+}
+
+
+public static class JsonFileUtils
+{
+    private static readonly JsonSerializerSettings _options
+        = new() { NullValueHandling = NullValueHandling.Ignore, };
+
+    public static void SimpleWrite(object obj, string fileName)
+    {
+        var jsonString = JsonConvert.SerializeObject(obj, _options);
+        File.WriteAllText(fileName, jsonString);
+    }
+}
+
+public enum Category
+{
+    indoor,
+    outdoor
+}
+
+public enum Gender
+{
+    male,
+    female
+}
---
+++ WorldAthleticsTableConverter/Program.cs
@@ -0,0 +1,139 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using UglyToad.PdfPig;
+using WorldAthleticsTableConverter;
+
+await Main.ParsePDF();
+
+public static class Main
+{
+
+    private static List<PointsPerEvent> PointsTable = new();
+
+    public static async Task ParsePDF()
+    {
+        Stopwatch stopWatch = new();
+        stopWatch.Start();
+        Console.WriteLine("Processing");
+        try
+        {
+            PointsTable.Clear();
+            using PdfDocument document = PdfDocument.Open(@"C:\Users\jkdsa\source\repos\WorldAthleticsTableConverter\WorldAthleticsTableConverter\PointsTable.pdf");
+            foreach (var page in document.GetPages())
+            {
+                Gender gender = new();
+                int index = 0;
+                List<string> eventNameList = new();
+                var wordsList = page.GetWords().GroupBy(row => row.BoundingBox.Bottom);
+                bool rowDirection = false; //if false points on the left 
+
+                if (page.GetWords().Count() < 200)//needs a check for if the page is not a table. 
+                    continue;
+
+                foreach (var row in wordsList)
+                {
+                    List<PointsPerEvent> events = new();
+                    var textRow = row.Select(x => x.Text).ToList();
+
+                    if (index == 0) gender = row.ElementAt(index).Text.ToLower() == "men’s" ? Gender.male : Gender.female;
+
+                    else if (index == 1)//Get the header
+                    {
+                        var pointsIndex = textRow.IndexOf("Points");
+                        if (pointsIndex == 0)
+                            rowDirection = true;
+                        textRow.Remove("Points");
+                        eventNameList = textRow;
+                    }
+                    else PointsTable.AddRange(await Task.Run(() => ProcessRow(textRow, eventNameList, gender, rowDirection)));
+
+                    index++;
+                }
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        PointsTable = PointsTable.OrderByDescending(x => x.Points).ToList();
+        stopWatch.Stop();
+        Console.WriteLine($"Done time elapsed {stopWatch.Elapsed}");
+        await WriteToJSON();
+    }
+
+    private static Task<List<PointsPerEvent>> ProcessRow(List<string> row, List<string> eventNameList, Gender gender, bool tableDirection)
+    {
+        int index = 0;
+        List<PointsPerEvent> events = new();
+        int pointsPerRow = 0;
+        var last = row.Last();
+        foreach (var word in row)
+        {
+            if (tableDirection)
+            {
+                if (index == 0)
+                    pointsPerRow = int.Parse(word);
+                else
+                {
+                    PointsPerEvent newEvent = new()
+                    {
+                        Event = eventNameList.ElementAt(index - 1),
+                        Gender = gender,
+                        Mark = ConvertTimeToInt(word),
+                        Points = pointsPerRow
+                    };
+
+                    newEvent.Category = Events.IndoorEvents.Contains(newEvent.Event) ? Category.indoor : Category.outdoor;
+
+                    if(newEvent.Mark != 0)
+                        events.Add(newEvent);
+                }
+                index++;
+            }
+            else
+            {
+                if (word == last)
+                    pointsPerRow = int.Parse(word);
+                else
+                    pointsPerRow = int.Parse(last);
+
+                PointsPerEvent newEvent = new()
+                {
+                    Event = eventNameList.ElementAt(0),
+                    Gender = gender,
+                    Mark = ConvertTimeToInt(word),
+                    Points = pointsPerRow
+                };
+
+                newEvent.Category = Events.IndoorEvents.Contains(newEvent.Event) ? Category.indoor : Category.outdoor;
+
+                if (newEvent.Mark != 0)
+                    events.Add(newEvent);
+
+                index++;
+            }
+        }
+        return Task.FromResult(events);
+    }
+
+    public static Task WriteToJSON()
+    {
+        var fileName = @"C:\Users\jkdsa\source\repos\WorldAthleticsTableConverter\WorldAthleticsTableConverter\WorldAthletics.json";
+        JsonFileUtils.SimpleWrite(PointsTable, fileName);
+        return Task.CompletedTask;
+    }
+
+    private static decimal ConvertTimeToInt(string input)
+    {
+        var yuh = input.Split(':');
+
+        if (input == "-" || input == " ")
+            return 0;
+
+        decimal seconds = Convert.ToDecimal(yuh.Last());
+        decimal minutes = yuh.Length == 2 ? Convert.ToDecimal(yuh.ElementAt(0)) * 60 : 0;
+        decimal hours = yuh.Length == 3 ? Convert.ToDecimal(yuh.ElementAt(1)) * 60 * 60 : 0;
+
+        return minutes + seconds + hours;
+    }
+}
